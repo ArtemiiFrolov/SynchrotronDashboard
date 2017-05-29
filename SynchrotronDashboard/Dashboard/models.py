ab33baf064d7fb23636a2cedf807cbe24be6734d
ArtemiiFrolov <arfr@live.ru>
2017-05-29 12:37:13 +0300

    models migrations made

--- SynchrotronDashboard/Dashboard/models.py
+++ SynchrotronDashboard/Dashboard/models.py
@@ -1,6 +1,33 @@
 from django.db import models
 from datetime import datetime
 
+class Organization(models.Model):
+    name=models.CharField(max_length=100)
+
+class Station(models.Model):
+    name=models.CharField(max_length=100)
+
+class Metodic(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.CharField(max_length=1000)
+
+class Rights(models.Model):
+    name=models.CharField(max_length=100)
+
+class Role(models.Model):
+    name=models.CharField(max_length=100)
+    rights=models.ManyToManyField(Rights)
+
+class User(models.Model):
+    name=models.CharField(max_length=100)
+    station=models.ManyToManyField(Station)
+    organization=models.ManyToManyField(Organization)
+    role=models.ForeignKey(Role)
+    special_rights=models.ManyToManyField(Rights)
+
+class Equipment (models.Model):
+    name=models.CharField(max_length=150)
+
 class Request(models.Model):
     #добавить файл с договором, файл с отчетом, статью
     name=models.CharField(max_length=200)
@@ -16,35 +43,9 @@ class Request(models.Model):
     equipment=models.ManyToManyField(Equipment)
     status=models.CharField(max_length=100)
 
-class Comment(models.Model)
+class Comment(models.Model):
     request=models.ForeignKey(Request)
     author=models.ForeignKey(User)
     time_written=models.TimeField(default=datetime.now)
     text=models.CharField(max_length=1000)
-
-class Organization(models.Model):
-    name=models.CharField(max_length=100)
-
-class Station(models.Model):
-    name=models.CharField(max_length=100)
-
-class Metodic(models.Model):
-    name=models.CharField(max_length=100)
-    description=models.CharField(max_length=1000)
-
-class User(models.Model):
-    name=models.CharField(max_length=100)
-    station=models.ManyToManyField(Station)
-    organization=models.ManyToManyField(Organization)
-    role=models.ForeignKey(Role)
-    special_rights=models.ManyToManyField(Rights)
-
-class Equipment (models.Model):
-    name=models.CharField(max_length=150)
-
-class Role(models.Model):
-    name=models.CharField(max_length=100)
-    rights=models.ManyToManyField(Rights)
-
-class Rights(model.Model)
-
+    request=models.ForeignKey(Request)

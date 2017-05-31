d5922314ab7747a8163d09c2327ffe9ee629f954
ArtemiiFrolov <arfr@live.ru>
2017-05-31 18:09:41 +0300

    models upgrade

--- SynchrotronDashboard/Dashboard/models.py
+++ SynchrotronDashboard/Dashboard/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from datetime import datetime
-
+#добавить календари и документы!!!
 class Organization(models.Model):
     name=models.CharField(max_length=100)
 
@@ -27,25 +27,59 @@ class User(models.Model):
 
 class Equipment (models.Model):
     name=models.CharField(max_length=150)
+	
+class CompleteStatus (models.Model):
+    name=models.CharField(max_length=100)
+
+class StageStatus(models.Model):
+    name=models.CharField(max_length=100)
+	
+class JournalStatus(models.Model):
+    name=models.CharField(max_length=100)
+	
+class EventsList(models.Model):
+    name=models.CharField(max_length=100)
 
 class Request(models.Model):
     #добавить файл с договором, файл с отчетом, статью
     name=models.CharField(max_length=200)
+    author=models.ForeignKey(User, related_name='requests_as_author')
     organizations=models.ManyToManyField(Organization, related_name='requests')
     serial=models.CharField(max_length=15)
     description=models.CharField(max_length=2000)
     time_needed=models.IntegerField(default=0)
-    time_start=models.TimeField(default=datetime.now)
-    time_end=models.TimeField(default=datetime.now)
+    time_start=models.DateTimeField(auto_now_add=True)
+    time_end=models.DateTimeField(auto_now_add=True)
     station=models.ForeignKey(Station, related_name='requests')
-    metodic=models.ManyToManyField(Metodic)
-    participant=models.ManyToManyField(User)
-    equipment=models.ManyToManyField(Equipment)
-    status=models.CharField(max_length=100)			
+    metodic=models.ManyToManyField(Metodic, related_name='requests')
+    participant=models.ManyToManyField(User, related_name='requests_as_participant')
+    equipment=models.ManyToManyField(Equipment, related_name='requests')
+    complete_status=models.ForeignKey(CompleteStatus,related_name='requests')
+    stage_status=models.ForeignKey(StageStatus,related_name='requests')	
 
+class PlaningExperiment (models.Model):
+    author=models.ForeignKey(User)
+    request=models.ForeignKey(Request,related_name='planning_experiments')
+    time_start=models.DateTimeField(auto_now_add=True)
+    time_end=models.DateTimeField(auto_now_add=True)
+    status=models.ForeignKey(JournalStatus,related_name='planning_experiments')	
+	
+class Experiment(models.Model):
+    request=models.ForeignKey(Request,related_name='experiments')
+    auhtor=models.ForeignKey(User) #добавить автоматическое заполнение из request
+    time_start=models.DateTimeField(auto_now_add=True)
+    time_end=models.DateTimeField(auto_now_add=True)
+    operator=models.ForeignKey(User, related_name='experiments')
+    metodic=models.ForeignKey(Metodic, related_name='experiments')
+	
+class Event(models.Model):
+    name=models.ForeignKey(EventsList)
+    time_start=models.DateTimeField(auto_now_add=True)
+    time_end=models.DateTimeField(auto_now_add=True)
+	
 class Comment(models.Model):
     request=models.ForeignKey(Request)
     author=models.ForeignKey(User)
-    time_written=models.TimeField(default=datetime.now)
+    time_written=models.DateTimeField(auto_now_add=True)
     text=models.CharField(max_length=1000)
     request=models.ForeignKey(Request)

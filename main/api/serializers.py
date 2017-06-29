e0ccfb21c773b99ab7cb10fc731c9229c2db3d11
Boris shishov <borisshishov@gmail.com>
2017-06-29 14:13:47 +0300

    Populated api views and serializers

--- main/api/serializers.py
+++ main/api/serializers.py
@@ -8,3 +8,99 @@ class StationSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Station
         fields = '__all__'
+
+
+class OrganizationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Organization
+        fields = '__all__'
+
+
+class ApproachSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Approach
+        fields = '__all__'
+
+
+class RightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Right
+        fields = '__all__'
+
+
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Role
+        fields = '__all__'
+
+
+class EquipmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Equipment
+        fields = '__all__'
+
+
+class CompleteStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.CompleteStatus
+        fields = '__all__'
+
+
+class StageStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.StageStatus
+        fields = '__all__'
+
+
+class JournalStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.JournalStatus
+        fields = '__all__'
+
+
+class EventsListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.EventsList
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.User
+        fields = '__all__'
+
+
+class ApplicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Application
+        fields = '__all__'
+
+
+class ExperimentPlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.ExperimentPlan
+        fields = '__all__'
+
+
+class ExperimentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Experiment
+        fields = '__all__'
+
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Event
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Comment
+        fields = '__all__'
+
+
+class ApplicationCounterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.ApplicationCounter
+        fields = '__all__'
--- main/api/viewsets.py
+++ main/api/viewsets.py
@@ -8,3 +8,99 @@ class StationViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = serializers.StationSerializer
     permission_classes = [permissions.IsAuthenticated, ]
 
+
+class OrganizationViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.Organization.objects.all()
+    serializer_class = serializers.OrganizationSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+
+
+class ApproachViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.Approach.objects.all()
+    serializer_class = serializers.ApproachSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+
+
+class RightViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.Right.objects.all()
+    serializer_class = serializers.RightSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+
+
+class RoleViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.Role.objects.all()
+    serializer_class = serializers.RoleSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+
+
+class EquipmentViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.Equipment.objects.all()
+    serializer_class = serializers.EquipmentSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+
+
+class CompleteStatusViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.CompleteStatus.objects.all()
+    serializer_class = serializers.CompleteStatusSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+
+
+class StageStatusViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.StageStatus.objects.all()
+    serializer_class = serializers.StageStatusSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+
+
+class JournalStatusViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.JournalStatus.objects.all()
+    serializer_class = serializers.JournalStatusSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+
+
+class EventsListViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.EventsList.objects.all()
+    serializer_class = serializers.EventsListSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.User.objects.all()
+    serializer_class = serializers.UserSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+
+
+class ApplicationViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.Application.objects.all()
+    serializer_class = serializers.ApplicationSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+
+
+class ExperimentPlanViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.ExperimentPlan.objects.all()
+    serializer_class = serializers.ExperimentPlanSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+
+
+class ExperimentViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.Experiment.objects.all()
+    serializer_class = serializers.ExperimentSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+
+
+class EventViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.Event.objects.all()
+    serializer_class = serializers.EventSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+
+
+class CommentViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+
+
+class ApplicationCounterViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.ApplicationCounter.objects.all()
+    serializer_class = serializers.ApplicationCounterSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+

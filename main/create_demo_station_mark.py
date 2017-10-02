334e63b0711c261838f86419b249945134cafb80
msmirnov91 <fourcross@yandex.ru>
2017-10-02 10:41:06 +0300

    demo stats script improvement

--- main/create_demo_station_mark.py
+++ main/create_demo_station_mark.py
@@ -10,11 +10,16 @@ from main.models import StationMark, StationMarkValue
 
 
 def linear_func(x):
-    return x
+    k = 1
+    b = 0
+    return k*x + b
 
 
 def square_func(x):
-    return x * x
+    a = -90
+    b = 0
+    c = 0
+    return a*(x**2) + b*x + c
 
 
 def sqrt_func(x):
@@ -24,14 +29,16 @@ def sqrt_func(x):
         return x**1/2
 
 
-def make_ten_points(mark_instance, distrib_law):
-    StationMarkValue.objects.filter(mark=mark_instance).delete()
+def make_ten_points(mark_name, distrib_law):
+    mark, _ = StationMark.objects.get_or_create(name=mark_name)
+
+    StationMarkValue.objects.filter(mark=mark).delete()
 
     for i in range(0, 10):
-        v = StationMarkValue.objects.create(mark=mark_instance, value=float(distrib_law(i)))
+        v = StationMarkValue.objects.create(mark=mark, value=float(distrib_law(i)))
         v.save()
 
 if __name__ == '__main__':
-    mark, _ = StationMark.objects.get_or_create(name="test1")
-    make_ten_points(mark, square_func)
+
+    make_ten_points("test1", square_func)
     print('created successfully')
